94ee88970aa4fdb3a0ed064b756b5651c6bda49e
Josh <camman3d@gmail.com>
2013-10-09 16:34:49 -0400

    Updated stepper to latest version

--- src/main/java/grader/sakai/project/ProjectStepper.java
+++ src/main/java/grader/sakai/project/ProjectStepper.java
@@ -1,46 +1,39 @@
 package grader.sakai.project;
 
+import util.annotations.ComponentWidth;
+import util.annotations.Row;
+import util.annotations.Visible;
 import util.misc.ClearanceManager;
 import util.models.PropertyListenerRegisterer;
+import grader.project.Project;
 
 import java.beans.PropertyChangeListener;
 
 
-public interface ProjectStepper extends ClearanceManager, PropertyListenerRegisterer, PropertyChangeListener {
-    public boolean setProject(SakaiProject newVal);
+public interface ProjectStepper  extends ClearanceManager, PropertyListenerRegisterer, PropertyChangeListener{
+	public boolean setProject(SakaiProject newVal) ;
+	
+	public void output();
+	
+	public void sources() ;
+	public double getScore() ;
+	public void setScore(double newVal) ;
+	public  void waitForClearance() ;
+	public SakaiProjectDatabase getProjectDatabase() ;
 
-    public void output();
-
-    public void sources();
-
-    public double getScore();
-
-    public void setScore(double newVal);
-
-    public void waitForClearance();
-
-    public SakaiProjectDatabase getProjectDatabase();
-
-    public void setProjectDatabase(SakaiProjectDatabase aProjectDatabase);
-
-    public void setOnyen(String anOnyen);
-
-    public boolean setProject(String anOnyen);
-
-    public boolean isAutoRun();
-
+	public void setProjectDatabase(SakaiProjectDatabase aProjectDatabase) ;
+	public void setOnyen(String anOnyen) ;
+	public boolean setProject(String anOnyen) ;
+	public boolean isAutoRun() ;
     public void setAutoRun(boolean newVal);
-
-    public void autoRun();
-
+    public void autoRun() ;
     public boolean hasMoreSteps();
-
-    public void setHasMoreSteps(boolean newVal);
-
+	
+	public void setHasMoreSteps(boolean newVal);
     public SakaiProject getProject();
+    void runProjectsInteractively();
+    public void configureNavigationList();
 
-    // Added this so different project steppers can work differently
-    public void display();
-
+	
 
 }
--- src/main/java/grader/sakai/project/SakaiProjectDatabase.java
+++ src/main/java/grader/sakai/project/SakaiProjectDatabase.java
@@ -1,64 +1,77 @@
 package grader.sakai.project;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+
 import grader.assignment.AssignmentDataFolder;
 import grader.assignment.GradingFeatureList;
 import grader.feedback.AutoFeedback;
 import grader.feedback.ManualFeedback;
 import grader.feedback.ScoreFeedback;
 import grader.feedback.SourceDisplayer;
+import grader.project.Project;
 import grader.sakai.BulkAssignmentFolder;
 import grader.spreadsheet.FeatureGradeRecorder;
 import grader.spreadsheet.FinalGradeRecorder;
 
-import java.util.Collection;
-import java.util.Set;
-
 public interface SakaiProjectDatabase {
-    public BulkAssignmentFolder getBulkAssignmentFolder();
+	public BulkAssignmentFolder getBulkAssignmentFolder();
+	
+	public AssignmentDataFolder getAssigmentDataFolder() ;
+	
+	public SakaiProject getProject(String aName) ;
+	public Set<String> getOnyens() ;
+	
+	public Collection<SakaiProject> getProjects();
+	public SakaiProject runProject(String anOnyen);
+	public void runProjectInteractively(String anOnyen);
+	
+	public void runProjectInteractively(String anOnyen, ProjectStepper projectStepper) ;
+	public void displayOutput();
+	public void runProjectsInteractively();
 
-    public AssignmentDataFolder getAssigmentDataFolder();
+	FinalGradeRecorder getGradeRecorder();
+	public GradingFeatureList getGradingFeatures();
 
-    public SakaiProject getProject(String aName);
+	FeatureGradeRecorder getFeatureGradeRecorder();
+	public void setGradeRecorder(FinalGradeRecorder gradeRecorder) ;
 
-    public Set<String> getOnyens();
+	public void setFeatureGradeRecorder(FeatureGradeRecorder featureGradeRecorder) ;
+	SakaiProject runProject(String anOnyen, SakaiProject aProject);
 
-    public Collection<SakaiProject> getProjects();
+	void resetRunningProject(SakaiProject aProject);
+	public FinalGradeRecorder getTotalScoreRecorder() ;
+	public void  setTotalScoreRecorder(FinalGradeRecorder newVal) ;
 
-    public SakaiProject runProject(String anOnyen);
+	public AutoFeedback getAutoFeedback() ;
+	
+	public ManualFeedback getManualFeedback();
+	
+	public ScoreFeedback getScoreFeedback() ;
 
-    public void runProjectInteractively(String anOnyen);
+	public SourceDisplayer getSourceDisplayer() ;
+	void initIO();
+	
+		
+	void recordWindows();
+	
+	void resetIO();
+	void clearWindows();
+	public List<String> getOnyenNavigationList(SakaiProjectDatabase aSakaiProjectDatabase);
+	public List<String> getOnyenNavigationList();
+	void nonBlockingRunProjectsInteractively();
+	public String getNavigationFilter() ;
 
-    public void runProjectInteractively(String anOnyen, ProjectStepper projectStepper);
+	public void setNavigationFilter(String navigationFilter) ;
 
-    public void displayOutput();
+	void startProjectStepper();
 
-    public void runProjectsInteractively();
+	public Object displayProjectStepper(ProjectStepper aProjectStepper) ;
 
-    FinalGradeRecorder getGradeRecorder();
 
-    public GradingFeatureList getGradingFeatures();
 
-    FeatureGradeRecorder getFeatureGradeRecorder();
-
-    public void setGradeRecorder(FinalGradeRecorder gradeRecorder);
-
-    public void setFeatureGradeRecorder(FeatureGradeRecorder featureGradeRecorder);
-
-    SakaiProject runProject(String anOnyen, SakaiProject aProject);
-
-    void resetRunningProject(SakaiProject aProject);
-
-    public FinalGradeRecorder getTotalScoreRecorder();
-
-    public void setTotalScoreRecorder(FinalGradeRecorder newVal);
-
-    public AutoFeedback getAutoFeedback();
-
-    public ManualFeedback getManualFeedback();
-
-    public ScoreFeedback getScoreFeedback();
-
-    public SourceDisplayer getSourceDisplayer();
 
 
 }
